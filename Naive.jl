91206a7e88bf88162a93a2150acc996c9bd462b9
PhilMurzynowski <filipjasper@gmail.com>
2021-04-27 19:34:44 -0400

    first commit

---
+++ Naive.jl
@@ -0,0 +1,19 @@
+"""
+
+Naive O(n^2) force calculation.
+Stores in arrays and uses builtin cumsum.
+
+"""
+
+
+# pass in forces and bodies 
+
+#function naiveForceCalculation(bodies, forces)
+#  2 + 2
+#end
+
+function add(x, y)
+  x+y
+end
+
+naiveForceCalculation
